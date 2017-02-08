080fed47196d3dfcbc5003bca2892494dd8fd567
TobyoTenma <tmtbo@hotmail.com>
2017-02-08 23:47:14 +0800

    1.完善 TTAHUD

--- UtilsDemo/UtilsDemo/Utils/Public/TTAHUD/TTAHUD.m
+++ UtilsDemo/UtilsDemo/Utils/Public/TTAHUD/TTAHUD.m
@@ -9,21 +9,197 @@
 #import "TTAHUD.h"
 #import "MBProgressHUD.h"
 
+@interface TTAHUD () {
+    MBProgressHUD *_syncHUD;
+}
+
+@end
+
 @implementation TTAHUD
 
++ (instancetype)sharedInstance {
+    static id instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[TTAHUD alloc] init];
+    });
+    return instance;
+}
+
+/*==================== Alert ========================*/
+// MARK:- Alert : 使用 UIAlertController+Block
+
+/*==================== Loading ========================*/
+// MARK:- Loading
+
+- (MBProgressHUD *)loading {
+    return [self loading:nil];
+}
+
+- (MBProgressHUD *)loading:(NSString *)msg {
+    return [self loading:msg inView:nil];
+}
+
 - (MBProgressHUD *)loading:(NSString *)msg inView:(UIView *)aView {
-    UIView *inView = aView ? aView : [AppDelegate sharedAppDelegate].window;
-    MBProgressHUD *hud = [[MBProgressHUD alloc] initWithView:inView];
+//    UIView *inView = aView ? aView : [AppDelegate sharedAppDelegate].window;
+//    MBProgressHUD *hud = [[MBProgressHUD alloc] initWithView:inView];
+    MBProgressHUD *hud = [self getHUDInView:aView];
     dispatch_async(dispatch_get_main_queue(), ^{
         if (![msg isEmpty]) {
             hud.mode = MBProgressHUDModeIndeterminate;
             hud.label.text = msg;
         }
-        [inView addSubview:hud];
+//        [inView addSubview:hud];
         [hud showAnimated:YES];
         [hud hideAnimated:YES afterDelay:kRequestTimeOutTime];
     });
     return hud;
 }
+
+- (void)loading:(NSString *)msg delay:(CGFloat)aDelay execute:(dispatch_block_t)exec completionHandler:(dispatch_block_t)completionHandler {
+    MBProgressHUD *hud = [self getHUDInView:[AppDelegate sharedAppDelegate].window];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (![msg isEmpty]) {
+            hud.mode = MBProgressHUDModeText;
+            hud.label.text = msg;
+        }
+        [hud showAnimated:YES];
+        if (exec) {
+            exec();
+        }
+        
+        [hud hideAnimated:YES afterDelay:aDelay];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(aDelay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            if (completionHandler) {
+                completionHandler();
+            }
+        });
+    });
+}
+
+- (MBProgressHUD *)getHUDInView:(UIView *)aView {
+    UIView *inView = aView ? aView : [AppDelegate sharedAppDelegate].window;
+    MBProgressHUD *hud = [[MBProgressHUD alloc] initWithView:inView];
+    [inView addSubview:hud];
+    return hud;
+}
+
+/*==================== Stop Loading ========================*/
+// MARK:- Stop Loading
+
+- (void)stopLoading:(MBProgressHUD *)hud {
+    [self stopLoading:hud message:nil];
+}
+
+- (void)stopLoading:(MBProgressHUD *)hud message:(NSString *)msg {
+    [self stopLoadig:hud message:msg delay:0 completionHandler:nil];
+}
+
+- (void)stopLoadig:(MBProgressHUD *)hud message:(NSString *)msg delay:(CGFloat)aDelay completionHandler:(dispatch_block_t)completionHandler {
+    if (hud && hud.superview) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (![msg isEmpty]) {
+                hud.mode = MBProgressHUDModeText;
+                hud.label.text = msg;
+            }
+            
+            [hud hideAnimated:YES afterDelay:aDelay];
+            _syncHUD = nil;
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(aDelay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                if (completionHandler) {
+                    completionHandler();
+                }
+            });
+        });
+    }
+}
+
+/*==================== Tip Message ========================*/
+// MARK:- Tip Message
+
+- (void)tipMessage:(NSString *)msg {
+    [self tipMessage:msg delay:2.0f];
+}
+
+- (void)tipMessage:(NSString *)msg delay:(CGFloat)aDelay {
+    [self tipMessage:msg delay:aDelay completionHandler:nil];
+}
+
+- (void)tipMessage:(NSString *)msg delay:(CGFloat)aDelay completionHandler:(dispatch_block_t)completionHandler {
+    if ([msg isEmpty]) {
+        return;
+    }
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        MBProgressHUD *hud = [self getHUDInView:nil];
+        hud.mode = MBProgressHUDModeText;
+        hud.label.text = msg;
+        [hud showAnimated:YES];
+        [hud hideAnimated:YES afterDelay:aDelay];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(aDelay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            if (completionHandler) {
+                completionHandler();
+            }
+        });
+    });
+}
+
+/*==================== Sync Loading ========================*/
+// MARK:- Sync Loading
+
+static const NSInteger kSyncHUDStartTag = 100000;
+
+- (void)syncLoading {
+    [self syncLoading:nil];
+}
+
+- (void)syncLoading:(NSString *)msg {
+    [self syncLoading:msg inView:nil];
+}
+
+- (void)syncLoading:(NSString *)msg inView:(UIView *)aView {
+    if (_syncHUD) {
+        _syncHUD.tag++;
+        if (![msg isEmpty]) {
+            _syncHUD.mode = MBProgressHUDModeText;
+            _syncHUD.label.text = msg;
+        } else {
+            _syncHUD.mode = MBProgressHUDModeIndeterminate;
+            _syncHUD.label.text = nil;
+        }
+        return;
+    }
+    _syncHUD = [self loading:msg inView:aView];
+    _syncHUD.tag = kSyncHUDStartTag;
+}
+
+/*==================== Sync Stop Loading ========================*/
+// MARK:- Sync Stop Loading
+
+- (void)syncStopLoading {
+    [self syncStopLoadingMessage:nil delay:0.f completionHandler:nil];
+}
+
+- (void)syncStopLoadingMessage:(NSString *)msg {
+    [self syncStopLoadingMessage:msg delay:1.f completionHandler:nil];
+}
+
+- (void)syncStopLoadingMessage:(NSString *)msg delay:(CGFloat)aDelay completionHandler:(dispatch_block_t)completionHandler {
+    _syncHUD.tag--;
+    if (_syncHUD.tag > kSyncHUDStartTag) {
+        if (![msg isEmpty]) {
+            _syncHUD.mode = MBProgressHUDModeText;
+            _syncHUD.label.text = msg;
+        } else {
+            _syncHUD.mode = MBProgressHUDModeIndeterminate;
+            _syncHUD.label.text = nil;
+        }
+    } else {
+        [self stopLoadig:_syncHUD message:msg delay:aDelay completionHandler:completionHandler];
+    }
+}
+
 
 @end
